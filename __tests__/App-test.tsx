e8cfbfd00cfceb8d03d64736fdbbc1e43ec91c7f
guilherme_urbanas <guilherme.urbanas@ocumetra.com>
2023-05-02 22:01:54 +0100

    ⚙️ React Native build/setup files

---
+++ __tests__/App-test.tsx
@@ -0,0 +1,15 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import { Root } from '../src/Root';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import { it } from 'node:test';
+
+it('renders correctly', () => {
+  renderer.create(<Root />);
+});
