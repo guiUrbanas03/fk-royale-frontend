820c8a2170210fb93db4eb35fbb01cde7b61e2d3
guilherme_urbanas <guilherme.urbanas@ocumetra.com>
2023-05-02 22:08:31 +0100

    💻 Create useUser hook

---
+++ src/hooks/user/user.ts
@@ -0,0 +1,38 @@
+import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query"
+import { UserResource, whoAmI } from "../../api/auth/auth";
+import { useAuthInterceptors } from "../auth/interceptors";
+
+const getUser = async () => {
+  const res = await whoAmI();
+  
+  if (res && res.status === 200) {
+    return res.data.user;
+  }
+
+  return null;
+}
+
+const useUser = () => {
+  const updateUser = (user: UserResource) => {
+    queryClient.setQueryData(["auth", "whoami"], user);
+  }
+
+  const clearUser = () => {
+    queryClient.setQueryData(["auth", "whoami"], null);
+  }
+
+  const queryClient: QueryClient = useQueryClient();
+  useAuthInterceptors(clearUser);
+
+  const { data: user } = useQuery({
+    queryKey: ["auth", "whoami"],
+    queryFn: () => getUser(),
+  });
+  
+  return { 
+    user, updateUser, clearUser
+  }
+
+}
+
+export { useUser };
