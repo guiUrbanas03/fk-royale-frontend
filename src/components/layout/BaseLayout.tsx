ebcb32cd04f783eaac7ed64d92327fb918f10097
guilherme_urbanas <guilherme.urbanas@ocumetra.com>
2023-05-07 18:04:01 +0100

    Create base layout component

---
+++ src/components/layout/BaseLayout.tsx
@@ -0,0 +1,48 @@
+import LinearGradient from "react-native-linear-gradient"
+import { useTheme } from "react-native-paper"
+import { ScrollView, StyleSheet, ViewStyle } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+type BaseLayoutProps = {
+    children: React.ReactNode
+}
+
+const BaseLayout = ({ children }: BaseLayoutProps): JSX.Element => {
+    const theme = useTheme();
+
+    return (
+        <LinearGradient
+            colors={[theme.colors.primary, theme.colors.secondary]} 
+            start={{x: 0, y: 0}} end={{x: 0, y: 1}}
+            style={styles.linearGradient}
+        >   
+            <SafeAreaView>
+                <ScrollView style={{ padding: 20}} contentInsetAdjustmentBehavior="automatic">
+                    { children }
+                </ScrollView>
+            </SafeAreaView>
+        {/* <View style={styles.cardsBackground}>
+            <CardsBackgroundSVG />
+        </View> */}
+        </LinearGradient>
+    )
+}
+
+type Styles = {
+    linearGradient: ViewStyle,
+    cardsBackground: ViewStyle,
+}
+
+const styles = StyleSheet.create<Styles>({
+    linearGradient: {
+      flex: 1,
+    },
+    cardsBackground: { 
+        position: 'absolute', 
+        bottom: 0,
+        opacity: 0.4,
+        zIndex: 0,
+    },
+});
+
+export { BaseLayout };
